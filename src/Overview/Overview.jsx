5edf62f64cb6a4945771fcfff1983d4d2f221bb9
armanrozika <armanrozika@gmail.com>
2022-01-25 10:03:58 +0800

    add middle overview section

--- src/Overview/Overview.jsx
+++ src/Overview/Overview.jsx
@@ -1,34 +1,40 @@
 import React from "react";
+import dataArranging from "../assets/dataArranging.png";
+import suaraTertinggi from "../assets/suaraTertinggi.png";
+import golput from "../assets/golput.png";
+import tidakSah from "../assets/tidakSah.png";
+import paslon from "../assets/paslon.png";
+import files from "../assets/files.png";
 
 function Overview() {
   return (
     <div>
       <h2 className="font-bold text-xl text-teal-400">Overview</h2>
-      <div className="grid grid-cols-4 gap-4 mt-8 bg-white p-5 rounded-lg shadow-lg shadow-gray-100">
+      <div className="grid grid-cols-4 gap-4 mt-8 bg-white p-2 rounded-lg shadow-lg shadow-gray-100">
         <div className="flex flex-col p-2 items-center border-r-2 border-slate-50">
-          <p className="text-sm mb-3 text-slate-500 font-bold">TPS 3</p>
-          <p className="flex items-center justify-center bg-orange-50 w-20 h-20 rounded-full text-teal-400 font-bold border-2 border-orange-100">
+          <p className="text-sm mb-2 text-slate-500 font-bold">TPS 3</p>
+          <p className="flex items-center justify-center bg-orange-50 w-16 h-16 rounded-full text-teal-400 font-bold border-2 border-orange-100 text-sm">
             250
           </p>
           <p className="mt-2 text-sm text-gray-400">Truno Joyo</p>
         </div>
         <div className="flex flex-col p-2 items-center border-r-2 border-slate-50">
-          <p className="text-sm mb-3 text-slate-500 font-bold">TPS 4</p>
-          <p className="flex items-center justify-center bg-orange-50 w-20 h-20 rounded-full text-teal-400 font-bold border-2 border-orange-100">
+          <p className="text-sm mb-2 text-slate-500 font-bold">TPS 4</p>
+          <p className="flex items-center justify-center bg-orange-50 w-16 h-16 rounded-full text-teal-400 font-bold border-2 border-orange-100  text-sm">
             12
           </p>
           <p className="mt-2 text-sm text-gray-400">Panjisari</p>
         </div>
         <div className="flex flex-col p-2 items-center border-r-2 border-slate-50">
-          <p className="text-sm mb-3 text-slate-500 font-bold">TPS 6</p>
-          <p className="flex items-center justify-center bg-orange-50 w-20 h-20 rounded-full text-teal-400 font-bold border-2 border-orange-100">
+          <p className="text-sm mb-2 text-slate-500 font-bold">TPS 6</p>
+          <p className="flex items-center justify-center bg-orange-50 w-16 h-16 rounded-full text-teal-400 font-bold border-2 border-orange-100 text-sm">
             78
           </p>
           <p className="mt-2 text-sm text-gray-400">Malabar Barat</p>
         </div>
         <div className="flex flex-col p-2 items-center ">
-          <p className="text-sm mb-3 text-slate-500 font-bold">TPS 9</p>
-          <p className="flex items-center justify-center bg-orange-50 w-20 h-20 rounded-full text-teal-400 font-bold border-2 border-orange-100">
+          <p className="text-sm mb-2 text-slate-500 font-bold">TPS 9</p>
+          <p className="flex items-center justify-center bg-orange-50 w-16 h-16 rounded-full text-teal-400 font-bold border-2 border-orange-100 text-sm">
             700
           </p>
           <p className="mt-2 text-sm text-gray-400">Bulaksumur Timur</p>
@@ -43,18 +49,18 @@ function Overview() {
         <div className="bg-white rounded-lg w-48 p-3 text-center shadow-lg shadow-gray-100">
           <div className="border-b-2 border-slate-50">
             <img
-              src="https://avatarfiles.alphacoders.com/249/249786.jpg"
+              src={paslon}
               alt=""
-              className="w-4/5 my-0 mx-auto rounded-full"
+              className="w-full my-0 mx-auto rounded-full"
             />
-            <p className="bg-orange-50 rounded-full text-sm py-1 mt-3 text-slate-600 font-bold">
+            <p className="bg-orange-50 rounded-full text-sm py-1 mt-0 text-slate-600 font-bold">
               12.750
             </p>
             <p className="mt-2 text-sm text-gray-500">Pasangan No. 3</p>
           </div>
           <div className="border-b-2 border-slate-50 mt-9">
             <img
-              src="https://avatarfiles.alphacoders.com/280/280205.jpg"
+              src={paslon}
               alt=""
               className="w-4/5 my-0 mx-auto rounded-full"
             />
@@ -66,7 +72,7 @@ function Overview() {
 
           <div className="border-b-2 border-slate-50 mt-9">
             <img
-              src="https://avatarfiles.alphacoders.com/532/53282.jpg"
+              src={paslon}
               alt=""
               className="w-4/5 my-0 mx-auto rounded-full"
             />
@@ -77,8 +83,34 @@ function Overview() {
           </div>
         </div>
 
-        <div className="bg-white w-full ml-8 rounded-lg shadow-lg shadow-gray-100 p-3">
-          <p>Mantap</p>
+        <div className="bg-white w-full ml-8 rounded-lg shadow-lg shadow-gray-100 px-20 py-10">
+          <div className="flex items-center border-b-2 border-teal-100">
+            <img className="w-24 mr-5" src={files} alt="data-koleksi" />
+            <h1 className="text-slate-500">
+              Pasangan calon Anda memperoleh suara tertinggi pada Kabupaten
+              Bontang Timur
+            </h1>
+          </div>
+          <div className="flex items-center border-b-2 border-teal-100">
+            <img className="w-24 mr-5" src={files} alt="data-koleksi" />
+            <h1 className="text-slate-500">
+              Pasangan calon Anda memperoleh suara terendah pada Kabupaten
+              Mamuju
+            </h1>
+          </div>
+          <div className="flex items-center border-b-2 border-teal-100">
+            <img className="w-24 mr-5" src={files} alt="data-koleksi" />
+            <h1 className="text-slate-500">
+              Dari keseluruhan pemilih terdaftar, 700 di antaranya golput/tidak
+              memilih
+            </h1>
+          </div>
+          <div className="flex items-center">
+            <img className="w-24 mr-5" src={files} alt="data-koleksi" />
+            <h1 className="text-slate-500">
+              Dari keseluruhan surat suara, terdapat sebanyak 35 suara tidak sah
+            </h1>
+          </div>
         </div>
       </div>
     </div>
